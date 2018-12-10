f650f62277aea7923c8cb6628649236f51e80ec5
Oluwasayofunmi Williams <sayofunmi_wllms@yahoo.com>
2018-12-10 14:10:28 +0000

    Block unwanted web content

--- Platter/Controller/RecipePageController.swift
+++ Platter/Controller/RecipePageController.swift
@@ -12,13 +12,18 @@ import WebKit
 class RecipePageController: UIViewController {
     
 
-    @IBOutlet weak var recipeWebPage: WKWebView!
+    
+    @IBOutlet var webView: WKWebView!
     
     var pageURL : String?
+    
+    var webview: WKWebView!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        contentBlock()
         
         if let loadURL = pageURL{
             
@@ -26,19 +31,72 @@ class RecipePageController: UIViewController {
             
             let request = URLRequest(url: url!)
             
-            recipeWebPage.load(request)
+            webView.load(request)
             
         }
-    
-        
-        
-        
-        
-
         // Do any additional setup after loading the view.
     }
     
+    
+    
+    
+    let json = """
+[
+    {
+        "trigger": {
+            "if-domain": ".*"
+        },
+        "action": {
+            "type": "css-display-none",
+            "selector": ".overlay"
+        }
+    },
+    {
+        "trigger": {
+            "url-filter": "://googleads\\\\.g\\\\.doubleclick\\\\.net.*"
+        },
+        "action": {
+            "type": "block"
+        }
+    }
+]
+"""
+    
+    
+    
+    
+    func contentBlock () {
+        
+        WKContentRuleListStore.default()
+            .compileContentRuleList(forIdentifier: "ContentBlockingRules",
+                                    encodedContentRuleList: json)
+            { (contentRuleList, error) in
+                guard let contentRuleList = contentRuleList,
+                    error == nil else {
+                        return
+                }
+                
+                let configuration = WKWebViewConfiguration()
+                configuration.userContentController.add(contentRuleList)
+                
+                self.webView = WKWebView(frame: self.view.bounds,
+                                         configuration: configuration)
+        }
+        
+        
+        
+    
+                
+        }
+        
+        
+        
+        
+        
+    }
 
    
 
-}
+
+
+
