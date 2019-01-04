44efc6ed8fedcb24117ad06ab41f0d2ffa1335e3
Oluwasayofunmi Williams <sayofunmi_wllms@yahoo.com>
2019-01-04 15:04:58 +0000

    Add sound to meal button

--- Platter/Controller/HomeScreenController.swift
+++ Platter/Controller/HomeScreenController.swift
@@ -7,17 +7,48 @@
 //
 
 import UIKit
+import AVFoundation
 
 class HomeScreenController: UIViewController {
+    
+    var audioPlayer : AVAudioPlayer?
     
     @IBOutlet weak var newMeal: UIButton!
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
     }
+    
+    @IBAction func newMeal(_ sender: UIButton) {
+        
+        playSound()
+    }
+    
+    func playSound() {
+        
+        do{
+            guard let soundURL = Bundle.main.url(forResource: "ButtonSound", withExtension: "mp3")else{ return}
+            
+            print ("File was found at \(soundURL)")
+            
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback, mode: AVAudioSession.Mode.default)
+            
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
+            
+            audioPlayer?.play()
+            
+        }catch let error{
+            
+            print ("CANT play file because of error: \(error.localizedDescription.debugDescription)")
+        }
+    
+    }
+    
+    
 
 
 }
